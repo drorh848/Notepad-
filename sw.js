f8585cb45e2f78e718ae87e5595ce2d315adcd6f
drorh848 <drorhh0@gmail.com>
2026-08-05 08:47:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* הפתקים שלי - Service Worker
    בכל העלאת גרסה חדשה: שנה את SW_VERSION והדפדפן יתעדכן לבד */
-const SW_VERSION = "2.3";
+const SW_VERSION = "2.4";
 const CACHE = "notepad-" + SW_VERSION;
 
 const CORE = [
